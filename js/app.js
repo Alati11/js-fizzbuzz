8b9274a370ebe06c1bd7581f6ad34ba431ef8514
Alati11 <peppealati@gmail.com>
2023-10-06 15:43:11 +0200

    add mult-five

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
+
 // - creare un ciclo per scrivere in console i numeri da 1 a 100.
+for (let i = 0; i < 100; i++) {
+    const n = i + 1;
+    console.log(n); 
+
 //     - SE i numeri sono multipli di 3 
 //         - stampare "Fizz"
+const mulThr = n % 3 
+    if ( mulThr === 0) {
+//  const mulTre = fizz;           
+    console.log('fizz');
+    }
+
 //     - Se i numeri sono multipli di 5 
 //         - stampare "Buzz"
+const mulFiv = n % 5 
+    if ( mulFiv === 0) {
+        console.log('buzz');
+    }
+
 //     - Se i numeri multipli sia di 3 che di 5 
 //         - stampare "FizzBuzz"    
+}   
+
+
