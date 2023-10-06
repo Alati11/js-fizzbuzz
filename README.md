e7dbe17bec03752551466b309f7c6942adafdd19
Alati11 <peppealati@gmail.com>
2023-10-06 17:00:41 +0200

    correct printing in console

--- README.md
+++ README.md
@@ -3,12 +3,13 @@
 Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 - creare un ciclo per scrivere in console i numeri da 1 a 100.
+- dichiaro variabili per multipli di 3 e di 5 
+     - Se i numeri multipli sia di 3 che di 5 
+        - stampare "FizzBuzz"
     - SE i numeri sono multipli di 3 
         - stampare "Fizz"
     - Se i numeri sono multipli di 5 
         - stampare "Buzz"
-    - Se i numeri multipli sia di 3 che di 5 
-        - stampare "FizzBuzz"
-        
+   
           
 
--- js/app.js
+++ js/app.js
@@ -2,30 +2,36 @@
 // - creare un ciclo per scrivere in console i numeri da 1 a 100.
 for (let i = 0; i < 100; i++) {
     let n = i + 1;
-
-
-//     - SE i numeri sono multipli di 3 
-//         - stampare "Fizz"
+    // - dichiaro variabili per multipli di 3 e di 5 
     let mulThr = n % 3; 
-    if ( mulThr === 0) {
-    console.log('fizz');      
-    } 
-//     - Se i numeri sono multipli di 5 
-//         - stampare "Buzz"
     let mulFiv = n % 5;
-    if ( mulFiv === 0) {
-        console.log('buzz');
-    }
 
 //     - Se i numeri multipli sia di 3 che di 5 
 //         - stampare "FizzBuzz" 
-// let mulThrFiv = (n % 3 && n % 5); 
-    let mulThrFiv 
     if (mulThr === 0 && mulFiv === 0) {
         console.log('fizzbuzz');
-    } else { console.log(n);
+
+ //     - SE i numeri sono multipli di 3 
+//         - stampare "Fizz"
+    }   else if( mulThr === 0) { 
+        console.log('fizz')
+
+//     - Se i numeri sono multipli di 5 
+//         - stampare "Buzz" 
+    }   else if( mulFiv === 0) {
+        console.log('buzz')
     }
+    else {
+        console.log(n)
+    }
+    
+    
+
+    
+
+
+
+    
+  
 
 }   
-
-
