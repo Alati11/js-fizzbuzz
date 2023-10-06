b88bbb69f0db1d3336c7e8a45831007580e68e3e
Alati11 <peppealati@gmail.com>
2023-10-06 16:22:42 +0200

    error fizzbuzz

--- js/app.js
+++ js/app.js
@@ -1,26 +1,31 @@
 
 // - creare un ciclo per scrivere in console i numeri da 1 a 100.
 for (let i = 0; i < 100; i++) {
-    const n = i + 1;
-    console.log(n); 
+    let n = i + 1;
+
 
 //     - SE i numeri sono multipli di 3 
 //         - stampare "Fizz"
-const mulThr = n % 3 
+    let mulThr = n % 3; 
     if ( mulThr === 0) {
-//  const mulTre = fizz;           
-    console.log('fizz');
-    }
-
+    console.log('fizz');      
+    } 
 //     - Se i numeri sono multipli di 5 
 //         - stampare "Buzz"
-const mulFiv = n % 5 
+    let mulFiv = n % 5;
     if ( mulFiv === 0) {
         console.log('buzz');
     }
 
 //     - Se i numeri multipli sia di 3 che di 5 
-//         - stampare "FizzBuzz"    
+//         - stampare "FizzBuzz" 
+// let mulThrFiv = (n % 3 && n % 5); 
+    let mulThrFiv 
+    if (mulThr === 0 && mulFiv === 0) {
+        console.log('fizzbuzz');
+    } else { console.log(n);
+    }
+
 }   
 
 
